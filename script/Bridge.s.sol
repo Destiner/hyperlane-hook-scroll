79d3156b8b23269991af9a211df083df42dfa0f9
Timur Badretdinov <destinerx@gmail.com>
2023-11-03 22:03:54 +0000

    Fix destination domain, add L2 hook deployment, fix message body

--- script/Bridge.s.sol
+++ script/Bridge.s.sol
@@ -11,36 +11,27 @@ import { BaseScript } from "./Base.s.sol";
 
 contract Bridge is BaseScript {
     address MAILBOX_ADDRESS = 0xfFAEF09B3cd11D9b20d1a19bECca54EEC2884766;
-    uint32 DESTINATION_DOMAIN = 11_155_111;
-    IPostDispatchHook SCROLL_HOOK = IPostDispatchHook(0x5248130913109c347695f3beA0682eeE42c2436F);
+    uint8 MESSAGE_VERSION = 3;
+    uint32 DESTINATION_DOMAIN = 534_351;
+    IPostDispatchHook SCROLL_HOOK = IPostDispatchHook(0x3b00426e8e0c8B582e303B083Fd7a0da7eceE386);
+    IPostDispatchHook SCROLL_L2_HOOK = IPostDispatchHook(0xA0dDC930B77774a798868aC634d2509e71CbC00F);
     IL2GasPriceOracle l2GasPriceOracle = IL2GasPriceOracle(0x247969F4fad93a33d4826046bc3eAE0D36BdE548);
     uint256 GAS_LIMIT = 120_000;
 
     uint256 amount = 0.000123 ether;
 
     function run() public broadcast {
-        address from = address(SCROLL_HOOK);
-        address to = msg.sender;
-        bytes memory message = abi.encodePacked(
-            uint8(0),
-            uint32(0),
-            uint32(0),
-            TypeCasts.addressToBytes32(from),
-            DESTINATION_DOMAIN,
-            TypeCasts.addressToBytes32(to),
-            bytes("0x")
-        );
-        address recipient = msg.sender;
-        bytes32 recipientBytes = TypeCasts.addressToBytes32(recipient);
+        bytes32 recipient = TypeCasts.addressToBytes32(msg.sender);
+        bytes memory messageBody = bytes("0x");
         uint16 variant = 1;
         uint256 fee = l2GasPriceOracle.estimateCrossDomainMessageFee(GAS_LIMIT);
         uint256 value = amount + fee;
         address refundAddress = 0x429b1c760DCEAe09D0967870C33abfd43aE8E2d1;
         bytes memory hookMetadata = abi.encodePacked(variant, value, GAS_LIMIT, refundAddress, amount);
         Mailbox mailbox = Mailbox(MAILBOX_ADDRESS);
-        uint256 quote = mailbox.quoteDispatch(DESTINATION_DOMAIN, recipientBytes, message, hookMetadata, SCROLL_HOOK);
+        uint256 quote = mailbox.quoteDispatch(DESTINATION_DOMAIN, recipient, messageBody, hookMetadata, SCROLL_HOOK);
         mailbox.dispatch{ value: amount + fee + quote }(
-            DESTINATION_DOMAIN, recipientBytes, message, hookMetadata, SCROLL_HOOK
+            DESTINATION_DOMAIN, recipient, messageBody, hookMetadata, SCROLL_HOOK
         );
     }
 }
--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,6 +2,7 @@
 pragma solidity >=0.8.21 <0.9.0;
 
 import { TypeCasts } from "hyperlane-monorepo/solidity/contracts/libs/TypeCasts.sol";
+import { MailboxClient } from "hyperlane-monorepo/solidity/contracts/client/MailboxClient.sol";
 
 import { ScrollHook } from "../src/ScrollHook.sol";
 import { ScrollIsm } from "../src/ScrollIsm.sol";
@@ -22,9 +23,9 @@ contract DeployIsm is BaseScript {
 contract DeployHook is BaseScript {
     address l1Messenger = 0x50c7d3e7f7c656493D1D76aaa1a836CedfCBB16A;
     address MAILBOX_ADDRESS = 0xfFAEF09B3cd11D9b20d1a19bECca54EEC2884766;
-    uint32 DESTINATION_DOMAIN = 11_155_111;
+    uint32 DESTINATION_DOMAIN = 534_351;
 
-    address scrollIsm = 0x65B7249f2D2b8EE2fCFD19f2ccE2D4522031BFb9;
+    address scrollIsm = 0x51AE1324100aC700323d0E0Bc307F3a2E410D68e;
 
     function run() public broadcast returns (ScrollHook hook) {
         hook = new ScrollHook(
@@ -37,10 +38,29 @@ contract DeployHook is BaseScript {
 }
 
 contract SetAuthorizedHook is BaseScript {
-    ScrollIsm ism = ScrollIsm(0x65B7249f2D2b8EE2fCFD19f2ccE2D4522031BFb9);
-    address hook = 0x5248130913109c347695f3beA0682eeE42c2436F;
+    ScrollIsm ism = ScrollIsm(0x51AE1324100aC700323d0E0Bc307F3a2E410D68e);
+    address hook = 0x3b00426e8e0c8B582e303B083Fd7a0da7eceE386;
 
     function run() public broadcast {
         ism.setAuthorizedHook(TypeCasts.addressToBytes32(hook));
+    }
+}
+
+contract DeployL2Hook is BaseScript {
+    address MAILBOX_ADDRESS = 0x3C5154a193D6e2955650f9305c8d80c18C814A68;
+    uint32 DESTINATION_DOMAIN = 534_351;
+    address ism = 0x51AE1324100aC700323d0E0Bc307F3a2E410D68e;
+    // We don't actually need this, but it's required to pass the "is contract" check
+    address messenger = 0xBa50f5340FB9F3Bd074bD638c9BE13eCB36E603d;
+
+    function run() public broadcast returns (ScrollHook hook) {
+        hook = new ScrollHook(
+            MAILBOX_ADDRESS,
+            DESTINATION_DOMAIN,
+            TypeCasts.addressToBytes32(ism),
+            messenger
+        );
+
+        hook.setInterchainSecurityModule(ism);
     }
 }
